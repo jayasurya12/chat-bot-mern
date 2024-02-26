c59ffe9fe8afa666343d5de45d8a2e46d50c2052
surya <jayasurya12vj@gmail.com>
2024-02-26 21:51:49 +0530

    feat: add auth context file in navigation

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import { AppBar, Toolbar } from '@mui/material';
 import Logo from './shared/Logo';
+import { useAuth } from '../context/AuthContext';
+import Navigationlink from './shared/Navigationlink';
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       sx={{ bgcolor: 'transparent', position: 'static', boxShadow: 'none' }}>
       <Toolbar sx={{ display: 'flex' }}>
         <Logo/>
+        <div>
+          { 
+          auth?.isLoggedIn ?( 
+            <>
+              <Navigationlink 
+                bg="#00fffc"
+                text="Go To Chat"
+                textColor='black'
+                to='/chat'
+              />
+              <Navigationlink 
+                bg="#51538f" 
+                textColor='white'
+                to='/'
+                text="Logout"
+                onClick={auth.logOut}
+              />
+            </> ) : 
+          <>
+            <Navigationlink 
+              bg="#00fffc"
+              text="Login"
+              textColor='black'
+              to='/login'
+            />
+            <Navigationlink 
+              bg="#51538f" 
+              textColor='white'
+              to='/signup'
+              text="signgUp"
+              onClick={auth.logOut}
+            />
+          </>
+        }
+        </div>
       </Toolbar>
     </AppBar>
   )
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -4,7 +4,6 @@ type User = {
     name: string,
     email: string
 }
-
 type UserAuth = {
     isLoggedIn: boolean;
     user: User | null;
@@ -12,17 +11,15 @@ type UserAuth = {
     signup: (name: string, email: string, password: string) => Promise<void>;
     logOut: () => Promise<void>;
 }
-
 const AuthContext = createContext<UserAuth | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
         // fetch if the user's cookies are valid then skip login
     }, []);
-
     const login = async (email: string, password: string) => {};
     const signup = async (name: string, email: string, password: string) => {};
     const logOut = async () => {};
@@ -43,7 +40,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 }
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
+    const context = ()=> useContext(AuthContext);
     if (!context) {
         throw new Error("useAuth must be used within an AuthProvider");
     }
